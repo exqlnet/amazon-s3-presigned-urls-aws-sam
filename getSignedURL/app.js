59f40b2e118b28257e609a2c7ddd3248a093d284
Exqlnet <44158642+exqlnet@users.noreply.github.com>
2024-05-10 18:03:37 +0800

    Update app.js

--- getSignedURL/app.js
+++ getSignedURL/app.js
@@ -1,18 +1,3 @@
-/*
-  Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-  Permission is hereby granted, free of charge, to any person obtaining a copy of this
-  software and associated documentation files (the "Software"), to deal in the Software
-  without restriction, including without limitation the rights to use, copy, modify,
-  merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
-  permit persons to whom the Software is furnished to do so.
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
-  INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
-  PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
-
 'use strict'
 
 const AWS = require('aws-sdk')
@@ -24,19 +9,23 @@ const URL_EXPIRATION_SECONDS = 300
 
 // Main Lambda entry point
 exports.handler = async (event) => {
-  return await getUploadURL(event)
+  const { queryStringParameters } = event;
+  const contentType = queryStringParameters && queryStringParameters.contentType ? queryStringParameters.contentType : 'image/jpeg';
+  return await getUploadURL(contentType);
 }
 
-const getUploadURL = async function(event) {
-  const randomID = parseInt(Math.random() * 10000000)
-  const Key = `${randomID}.jpg`
+const getUploadURL = async function(contentType) {
+  const timestamp = new Date().getTime();
+  const randomID = `${timestamp}_${parseInt(Math.random() * 10000000)}`; // Adding timestamp to randomID
+  const extension = contentType.split('/').pop(); // Extracting extension from contentType
+  const Key = `${randomID}.${extension}`; // Constructing objectKey using randomID and extension
 
   // Get signed URL from S3
   const s3Params = {
     Bucket: process.env.UploadBucket,
     Key,
     Expires: URL_EXPIRATION_SECONDS,
-    ContentType: 'image/jpeg',
+    ContentType: contentType,
 
     // This ACL makes the uploaded object publicly readable. You must also uncomment
     // the extra permission for the Lambda function in the SAM template.
